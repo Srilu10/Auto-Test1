6bf5e589af5b55352484b328070e607779fb2a7d
[GIT Srilu10] <varadi.narayanarao@gmail.com>
2017-11-29 01:29:16 +0530

    My Project

---
+++ myapp/src/main/java/com/varadi/app/App.java
@@ -0,0 +1,13 @@
+package com.varadi.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
